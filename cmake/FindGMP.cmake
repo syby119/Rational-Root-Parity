1a44ccd642dae045906fd89b71dbc89c6f1b9ba7
syby119 <syby119@126.com>
2023-12-08 05:33:22 +0800

    Fix find gmp cmake

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,79 +1,62 @@
-# Try to find the GNU Multiple Precision Arithmetic Library (GMP)
-# See http://gmplib.org/
+# Originally copied from the KDE project repository:
+# http://websvn.kde.org/trunk/KDE/kdeutils/cmake/modules/FindGMP.cmake?view=markup&pathrev=675218
 
-find_path(GMP_INCLUDES
-    NAMES
-        gmp.h
-    PATHS
-        ENV GMP_DIR
-        ${GMP_WINDOWS_PATH}
-        ${INCLUDE_INSTALL_DIR}
-    PATH_SUFFIXES
-        include
-)
+# Copyright (c) 2006, Laurent Montel, <montel@kde.org>
+# Copyright (c) 2007, 2008 Francesco Biscani, <bluescarni@gmail.com>
 
-find_library(GMP_LIBRARIES
-    NAMES
-        gmp
-        libgmp
-    PATHS
-        ENV GMP_DIR
-        ${GMP_WINDOWS_PATH}
-        ${LIB_INSTALL_DIR}
-    PATH_SUFFIXES
-        lib
-)
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ------------------------------------------------------------------------------------------
 
-set(GMP_EXTRA_VARS "")
-if(WIN32)
-    # Find dll file and set IMPORTED_LOCATION to the .dll file
-    find_file(GMP_RUNTIME_LIB
-        NAMES
-            gmp.dll
-            libgmp-10.dll
-        PATHS
-            ENV GMP_DIR
-            ${GMP_WINDOWS_PATH}
-            ${LIB_INSTALL_DIR}
-        PATH_SUFFIXES
-            lib
-    )
-    list(APPEND GMP_EXTRA_VARS GMP_RUNTIME_LIB)
-endif()
+# Try to find the GMP librairies:
+# GMP_FOUND - System has GMP lib
+# GMP_INCLUDE_DIR - The GMP include directory
+# GMP_LIBRARIES - Libraries needed to use GMP
+# GMPXX_INCLUDE_DIR - The GMP C++ interface include directory
+# GMPXX_LIBRARIES - Libraries needed to use GMP's C++ interface
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP
-    REQUIRED_VARS
-        GMP_INCLUDES
-        GMP_LIBRARIES
-        ${GMP_EXTRA_VARS}
-    REASON_FAILURE_MESSAGE
-        "GMP is not installed on your system. Install GMP using your preferred package manager."
-)
-mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
+IF(GMP_INCLUDE_DIR AND GMP_LIBRARIES AND GMPXX_INCLUDE_DIR AND GMPXX_LIBRARIES)
+	# Already in cache, be silent
+	SET(GMP_FIND_QUIETLY TRUE)
+ENDIF(GMP_INCLUDE_DIR AND GMP_LIBRARIES AND GMPXX_INCLUDE_DIR AND GMPXX_LIBRARIES)
 
-if(GMP_INCLUDES AND GMP_LIBRARIES AND NOT TARGET gmp::gmp)
-    if(GMP_RUNTIME_LIB)
-        add_library(gmp::gmp SHARED IMPORTED)
-    else()
-        add_library(gmp::gmp UNKNOWN IMPORTED)
-    endif()
+FIND_PATH(GMP_INCLUDE_DIR NAMES gmp.h)
+FIND_LIBRARY(GMP_LIBRARIES NAMES gmp)
+FIND_PATH(GMPXX_INCLUDE_DIR NAMES gmpxx.h)
+FIND_LIBRARY(GMPXX_LIBRARIES NAMES gmpxx)
 
-    # Set public header location and link language
-    set_target_properties(gmp::gmp PROPERTIES
-        IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-        INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDES}"
-    )
+IF(GMP_INCLUDE_DIR AND GMP_LIBRARIES AND GMPXX_INCLUDE_DIR AND GMPXX_LIBRARIES)
+	SET(GMP_FOUND TRUE)
+ENDIF(GMP_INCLUDE_DIR AND GMP_LIBRARIES AND GMPXX_INCLUDE_DIR AND GMPXX_LIBRARIES)
 
-    # Set lib location. On Windows we specify both the .lib and the .dll paths
-    if(GMP_RUNTIME_LIB)
-        set_target_properties(gmp::gmp PROPERTIES
-            IMPORTED_IMPLIB "${GMP_LIBRARIES}"
-            IMPORTED_LOCATION "${GMP_RUNTIME_LIB}"
-        )
-    else()
-        set_target_properties(gmp::gmp PROPERTIES
-            IMPORTED_LOCATION "${GMP_LIBRARIES}"
-        )
-    endif()
-endif()
+IF(GMP_FOUND)
+	IF(NOT GMP_FIND_QUIETLY)
+		MESSAGE(STATUS "Found GMP: ${GMP_LIBRARIES}, ${GMPXX_LIBRARIES}")
+	ENDIF(NOT GMP_FIND_QUIETLY)
+ELSE(GMP_FOUND)
+	IF(GMP_FIND_REQUIRED)
+		MESSAGE(FATAL_ERROR "Could NOT find GMP")
+	ENDIF(GMP_FIND_REQUIRED)
+ENDIF(GMP_FOUND)
+
+MARK_AS_ADVANCED(GMP_INCLUDE_DIR GMP_LIBRARIES GMPXX_INCLUDE_DIR GMPXX_LIBRARIES)
